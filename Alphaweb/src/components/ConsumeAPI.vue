faa2e2e30897612f4e6a0fb6766c314bc3f17cc5
Lucas <vanni.emails@proton.me>
2024-03-07 02:18:00 -0300

    fix: buttons

--- Alphaweb/src/components/ConsumeAPI.vue
+++ Alphaweb/src/components/ConsumeAPI.vue
@@ -194,4 +194,19 @@ const editProduct = async (data) => {
   width: 100px;
   height: auto;
 }
+
+button {
+  background-color: #007bff;
+  color: white;
+  padding: 10px 15px;
+  margin: 2px 5px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
 </style>
